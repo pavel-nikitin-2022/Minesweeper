bd4fa64b9c04053a840569b4d2786a7999121b9d
pavel-nikitin-2022 <90633463+pavel-nikitin-2022@users.noreply.github.com>
2023-03-05 21:03:31 +0300

    change win condition

--- src/utils/isVictory.ts
+++ src/utils/isVictory.ts
@@ -6,7 +6,11 @@ import { CellStatus, Cell as ICell, CellSprite } from "src/types";
  * @returns победил или нет
  */
 export function isVictory(array: ICell[]) {
-  const findEmpty = array.find((cell) => cell.status === CellStatus.Close);
+  const findEmpty = array.find(
+    (cell) =>
+      cell.status === CellStatus.Close ||
+      cell.sprite === CellSprite.QuestionMark
+  );
   const findBomb = array.find(
     (cell) => cell.sprite === CellSprite.ExplodedBomb
   );
